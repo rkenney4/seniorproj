6c8ae19b2b0be52010428f02fcaed9ed0b0637a4
SalielD <71668641+SalielD@users.noreply.github.com>
2022-02-15 15:14:48 -0500

    Added some CSS styling for the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <form>
-    <button type="submit" style="margin:15px">Sign Out</button>
-    <button type="submit" style="margin:15px">User Details</button>
-    <button type="submit" style="margin:15px">User Management</button>
+    <form class="home">
+    <button class="btn-1" type="submit" style="margin:15px">Sign Out</button>
+    <button class="btn-2" type="submit" style="margin:15px">User Details</button>
+    <button class="btn-3" type="submit" style="margin:15px">User Management</button>
   </form>
   
 </template>
