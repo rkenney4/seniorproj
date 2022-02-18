1d2aca0f9e2a1d35205f49879dea27c54bfb8f9d
Joey Pio <54558640+JP207746@users.noreply.github.com>
2022-02-18 10:41:23 -0500

    roles update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,84 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/feed">Feed</router-link> |
+    <router-link to="/about">Details</router-link> |
+    <router-link to="/feed">User Management</router-link> |
     <router-link to="/register">Register</router-link> |
     <router-link to="/sign-in">Login</router-link> 
-    
+    <button @click="handleSignOut">Sign Out</button>
   </div>
   <router-view />
 </template>
 
+<script setup>
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
+const isLoggedIn = ref(false);
+
+let auth;
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+        isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }    
+  });
+});
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push("/");
+  }).catch((error) => {
+  // An error happened.
+});
+};
+
+//role permissions
+const roles = 0;
+const userSignedIn = false; // user is not signed into the program
+const userActive = false; // user is not able to access the program
+const userManagement = false; // user is not able to access/edit the 'User Management' page
+const projectManagement = false; // user is not able to access/edit the 'User Management' page
+const message = "";
+
+/*
+switch (roles) {
+  case 1: // Super Admin
+
+    userActive = true;
+    userManagement = true;
+    projectManagement = true;
+    message = "Super Admin";
+    break;
+    
+  case 2: // Admin
+    userActive = true;
+    userManagement = false;
+    projectManagement = true;
+    message = "Admin";
+    break;
+
+  case 3: // Tester
+    userActive = true;
+    userManagement = false;
+    projectManagement = false;
+    message = "Tester";
+    break;
+
+  default: // Unassigned
+    userActive = false;
+    userManagement = false;
+    projectManagement = false;
+    message = "Unassigned";
+    break;
+}
+*/
+</script>
 
 <style>
 /*
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,25 @@
+
 <template>
   <head>
   </head>
   <body>
     <img src="../assets/OU-Logo.jpg" alt="OU-Logo" width="200" height="150">
+
+  <h1>
+    <b>Welcome to our Quality Assurance Management Team!</b>
+  </h1>
+
+  <p>
+    Team members: Joey, Saliel, Sarah, Maurice, and Richard
+  </p>
+  <p>
+    message
+  </p>
+  <form class="home">
+    <button class="btn-1" type="submit" style="margin:15px">Sign Out</button>
+    <button class="btn-2" type="submit" style="margin:15px">User Details</button>
+    <button class="btn-3" type="submit" style="margin:15px">User Management</button>
+  </form>
   
     <h1>
       <b>Welcome to our Quality Assurance Management Team!</b>
@@ -25,7 +42,8 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import {roles, userSignedIn, userActive, userManagement, projectManagement, message} from "../App.vue"
+//roles = 0;
 export default {
   name: "Home",
   components: {
