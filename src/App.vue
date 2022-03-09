edb651da3214e4105a7ee1a014d2c27820eede01
SarahWozniak <78500395+SarahWozniak@users.noreply.github.com>
2022-03-09 12:50:01 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,41 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/feed">Feed</router-link> |
     <router-link to="/register">Register</router-link> |
-    <router-link to="/sign-in">Login</router-link> 
+    <router-link to="/sign-in">Login</router-link> |
+    <button @click="handleSignOut" v-if="isLoggedIn">Sign Out</button>
     
   </div>
   <router-view />
 </template>
 
+<script setup>
+    import { onMounted, ref } from "vue";
+    import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+    import { useRouter } from "vue-router";
 
+    const router = useRouter();
+    const isLoggedIn = ref(false);
+
+    let auth;
+    onMounted(() => {
+        auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                isLoggedIn.value = true;
+            } else {
+                isLoggedIn.value = false;
+            }
+        });
+
+    });
+
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            router.push("/");
+        });
+    };
+
+</script>
 
 <style>
 #app {
