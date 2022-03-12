b19786a2cbd31330f096ee57b9e11fd85a9883ae
SarahWozniak <78500395+SarahWozniak@users.noreply.github.com>
2022-03-12 12:16:17 -0500

    Summart
    Made looking at certain pages available only to logged in users

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -18,16 +18,54 @@ const router = createRouter({
     {
       path: "/user-details",
       component: () => import("../views/UserDetails.vue"),
+      meta:{
+        requiresAuth: true,
+      },
     },
     {
       path: "/project-management",
       component: () => import("../views/ProjectManagement.vue"),
+      meta:{
+        requiresAuth: true,
+      },
     },
     {
       path: "/user-management",
       component: () => import("../views/UserManagement.vue"),
+      meta:{
+        requiresAuth: true,
+      },
     },
   ],
+});
+
+const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const removeListener = onAuthStateChanged(
+      getAuth(),
+      (user) => {
+        removeListener();
+        resolve(user);
+      },
+
+      reject
+    );
+  });
+};
+
+router.beforeEach(async (to, from, next) => {
+  if(to.matched.some((record) => record.meta.requiresAuth)){
+    if(await getCurrentUser()){
+      next();
+    } else{
+      alert("You do not have access to this page!");
+      next("/");
+    }
+
+  } else {
+    next();
+  }
+
 });
 
 export default router;
