619c5035e29f94c8b6e2d52a60db20f076d1bf57
Joey Pio <54558640+JP207746@users.noreply.github.com>
2022-04-11 13:15:20 -0400

    Updated login.js cypress script

--- tests/e2e/specs/login.js
+++ tests/e2e/specs/login.js
@@ -2,17 +2,76 @@
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.visit('https://seniorproj-d8a09.web.app/')
+    cy.visit('https://seniorproj-d8a09.web.app/sign-in')
   })
+  it('Failed Login Attempt #1', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
+  it('Failed Login Attempt #2', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
+  it('Failed Login Attempt #3', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
+  it('Failed Login Attempt #4', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
+  it('Failed Login Attempt #5', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
+  it('Failed Login Attempt #5', () => {
+    // https://on.cypress.io/type
+    cy.get('input').first()
+      .type('test@gmail.com').should('have.value', 'test@gmail.com')
+    cy.get('input').last()
+      .type('password1').should('have.value', 'password1')
+    cy.get('button').eq(7)
+      .click();
+    cy.wait(6000)
+  }),
    it('login', () => {
     // https://on.cypress.io/type
-    cy.get('button').eq(5)
-      .click();
     cy.get('input').first()
       .type('josephpio@oakland.edu').should('have.value', 'josephpio@oakland.edu')
     cy.get('input').last()
       .type('password').should('have.value', 'password')
     cy.get('button').eq(1)
       .click();
+      cy.wait(6000)
   })
  })
